2924ebcc67e04278fd9ed6e9a1304871766119bc
Luca5Eckert <ecklukatv@gmail.com>
2025-08-18 16:12:42 -0300

    feat: adiciona GeradorCodigo

--- src/com/prova/lucas/infra/persistencia/GeradorCodigo.java
+++ src/com/prova/lucas/infra/persistencia/GeradorCodigo.java
@@ -3,7 +3,7 @@ package com.prova.lucas.infra.persistencia;
 public class GeradorCodigo {
     public static int contagem = 0;
 
-    public static String gerarCodigo(char character){
-        return "" + character + contagem;
+    public String gerarCodigo(String sigla){
+        return "" + sigla + contagem;
     }
 }
