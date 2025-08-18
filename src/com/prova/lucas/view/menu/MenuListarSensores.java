15b466891c6fd4fc539bb76519588bef22828df2
Luca5Eckert <ecklukatv@gmail.com>
2025-08-18 16:35:54 -0300

    feat: finaliza MenuListarSensores

--- src/com/prova/lucas/view/menu/MenuListarSensores.java
+++ src/com/prova/lucas/view/menu/MenuListarSensores.java
@@ -1,7 +1,10 @@
 package com.prova.lucas.view.menu;
 
 import com.prova.lucas.controller.SensorController;
+import com.prova.lucas.dto.SensorResponse;
 import com.prova.lucas.view.Leitor;
+
+import java.util.List;
 
 public class MenuListarSensores extends Menu<String>{
 
@@ -14,15 +17,30 @@ public class MenuListarSensores extends Menu<String>{
 
     @Override
     public void chamarMenu() {
+        List<SensorResponse> sensores = sensorController.pegarSensores();
         System.out.println("===========================================");
         System.out.println("            Listar Sensores ");
         System.out.println("===========================================");
+        int i = 0;
+        sensores.forEach(System.out::println);
 
+        System.out.println("-------------------------------------------");
 
+        System.out.println(" S- Sair");
+        System.out.println(" A- Atualizar");
+
+        System.out.println("===========================================");
+
+        setAcao(getLeitor().lerLinha().trim().toUpperCase());
     }
 
     @Override
     public void executarMenu() {
-
+        setProximoMenu(
+                switch (getAcao()){
+                    case "S" -> new MenuPrincipal(getLeitor());
+                    default -> this;
+                }
+        );
     }
 }
