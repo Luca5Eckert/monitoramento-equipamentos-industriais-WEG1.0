18ab81dc4ba1b64373b6b12d54a490d1e1b24e82
Luca5Eckert <ecklukatv@gmail.com>
2025-08-18 16:35:17 -0300

    fix: arruma logica de adição no GeradorCodigo

--- src/com/prova/lucas/infra/persistencia/GeradorCodigo.java
+++ src/com/prova/lucas/infra/persistencia/GeradorCodigo.java
@@ -3,7 +3,8 @@ package com.prova.lucas.infra.persistencia;
 public class GeradorCodigo {
     public static int contagem = 0;
 
-    public String gerarCodigo(String sigla){
+    public synchronized String gerarCodigo(String sigla){
+        contagem++;
         return "" + sigla + contagem;
     }
 }
