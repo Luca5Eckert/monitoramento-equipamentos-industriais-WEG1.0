3801ae8222026e8d9604c98f3dd4c40b92a61026
Luca5Eckert <ecklukatv@gmail.com>
2025-08-18 16:35:43 -0300

    feat: adiciona toString no SensorResponse

--- src/com/prova/lucas/dto/SensorResponse.java
+++ src/com/prova/lucas/dto/SensorResponse.java
@@ -3,4 +3,10 @@ package com.prova.lucas.dto;
 import com.prova.lucas.model.enumerator.TipoSensor;
 
 public record SensorResponse(String codigo, String nomeEquipamento, TipoSensor tipoSensor) {
+
+    @Override
+    public String toString(){
+        return "Código: " + codigo + " | " + "Tipo: " + tipoSensor + " | " + "Equipamento: " + nomeEquipamento;
+    }
+
 }
