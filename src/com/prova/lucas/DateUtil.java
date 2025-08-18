ca09109dd43805345e93984fac1ba455983b4235
Luca5Eckert <ecklukatv@gmail.com>
2025-08-18 14:19:49 -0300

    feat(model): adiciona classe para lidar com formatação da datas

---
+++ src/com/prova/lucas/DateUtil.java
@@ -0,0 +1,13 @@
+package com.prova.lucas;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class DateUtil {
+
+    public static DateTimeFormatter pegarFormatacaoData(){
+        FormatStyle formatStyle = FormatStyle.SHORT;
+        return DateTimeFormatter.ofLocalizedDateTime(formatStyle);
+    }
+
+}
