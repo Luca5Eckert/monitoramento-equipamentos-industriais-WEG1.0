20d1e3b57bd44b6e09113cc0ec826dd1e1bbc2b0
Luca5Eckert <ecklukatv@gmail.com>
2025-08-18 16:14:26 -0300

    feat: adiciona SensorRepositorio

---
+++ src/com/prova/lucas/infra/persistencia/repository/SensorRepositorio.java
@@ -0,0 +1,25 @@
+package com.prova.lucas.infra.persistencia.repository;
+
+import com.prova.lucas.model.Sensor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class SensorRepositorio {
+
+    private List<Sensor> sensores = new ArrayList<>();
+
+    public void adicionar(Sensor sensor){
+        sensores.add(sensor);
+    }
+
+    public Optional<Sensor> pegar(String codigo){
+        for(Sensor sensor : sensores){
+            if(sensor.getCodigo().equals(codigo)){
+                return Optional.of(sensor);
+            }
+        }
+        return Optional.empty();
+    }
+}
