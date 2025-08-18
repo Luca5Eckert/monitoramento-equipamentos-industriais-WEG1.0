40b50b62c2742b7f9a706c6a22e6b8b42be97c3a
Luca5Eckert <ecklukatv@gmail.com>
2025-08-18 16:16:40 -0300

    feat: finaliza menu de cadastrar senores

--- src/com/prova/lucas/view/menu/MenuCadastrarSensor.java
+++ src/com/prova/lucas/view/menu/MenuCadastrarSensor.java
@@ -1,12 +1,18 @@
 package com.prova.lucas.view.menu;
 
+import com.prova.lucas.controller.SensorController;
 import com.prova.lucas.dto.SensorRequest;
+import com.prova.lucas.dto.SensorResponse;
+import com.prova.lucas.model.enumerator.TipoSensor;
 import com.prova.lucas.view.Leitor;
 
 public class MenuCadastrarSensor extends Menu<SensorRequest>{
 
-    public MenuCadastrarSensor(Leitor leitor) {
+    private final SensorController sensorController;
+
+    public MenuCadastrarSensor(Leitor leitor, SensorController sensorController) {
         super(leitor);
+        this.sensorController = sensorController;
     }
 
     @Override
@@ -17,12 +23,20 @@ public class MenuCadastrarSensor extends Menu<SensorRequest>{
         System.out.println(" Nome do equipamento: ");
         String nomeEquipamento = getLeitor().lerLinha();
 
-        System.out.println();
+        System.out.println(" Tipo do sensor: ");
+        TipoSensor.listarSensores();
+        TipoSensor tipoSensor = TipoSensor.pegarTipoSensor(getLeitor().lerNumeroInteiro());
 
+        System.out.println("===========================================");
+        SensorRequest sensorRequest = new SensorRequest(nomeEquipamento, tipoSensor);
+        setAcao(sensorRequest);
     }
 
     @Override
     public void executarMenu() {
-
+        SensorResponse sensorResponse = sensorController.adicionarSensor(getAcao());
+        System.out.println("Sensor cadastrado com sucesso!");
+        System.out.println("Tipo: " + sensorResponse.tipoSensor() + " | Limite de alerta: " + sensorResponse.tipoSensor().getLimiteAlerta() );
+        setProximoMenu(new MenuPrincipal(getLeitor()));
     }
 }
