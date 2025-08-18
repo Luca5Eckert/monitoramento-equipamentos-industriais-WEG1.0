07fdc2e8ca6e5993faec54c4f78ac3df082b22ec
Luca5Eckert <ecklukatv@gmail.com>
2025-08-18 15:10:50 -0300

    feat: adiciona construtores para classe sensores

--- src/com/prova/lucas/modal/SensorTemperatura.java
+++ src/com/prova/lucas/modal/SensorTemperatura.java
@@ -3,8 +3,14 @@ package com.prova.lucas.modal;
 import com.prova.lucas.exception.SensorLimiteMedicaoException;
 import com.prova.lucas.modal.enumerator.TipoSensor;
 
+import java.util.List;
+
 public class SensorTemperatura extends Sensor{
 
+
+    public SensorTemperatura(String codigo, String nomeEquipamento, List<Medicao> medicoes) {
+        super(codigo, nomeEquipamento, medicoes);
+    }
 
     @Override
     TipoSensor pegarTipoSensor() {
--- src/com/prova/lucas/modal/SensorVibracao.java
+++ src/com/prova/lucas/modal/SensorVibracao.java
@@ -2,8 +2,13 @@ package com.prova.lucas.modal;
 
 import com.prova.lucas.modal.enumerator.TipoSensor;
 
+import java.util.List;
+
 public class SensorVibracao extends Sensor{
 
+    public SensorVibracao(String codigo, String nomeEquipamento, List<Medicao> medicoes) {
+        super(codigo, nomeEquipamento, medicoes);
+    }
 
     @Override
     public TipoSensor pegarTipoSensor() {
