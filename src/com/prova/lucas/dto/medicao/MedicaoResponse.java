8e9687dbad2a86d2fabf530f2ffb844a25c733aa
Luca5Eckert <ecklukatv@gmail.com>
2025-08-22 14:33:20 -0300

    feat: adiciona to String para MedicaoResponse

--- src/com/prova/lucas/dto/medicao/MedicaoResponse.java
+++ src/com/prova/lucas/dto/medicao/MedicaoResponse.java
@@ -3,4 +3,13 @@ package com.prova.lucas.dto.medicao;
 import com.prova.lucas.model.enumerator.TipoSensor;
 
 public record MedicaoResponse(double valor, String data, TipoSensor tipoSensor, boolean alerta) {
+
+
+    @Override
+    public String toString() {
+        String mensagem = "";
+        if(alerta) mensagem = " ALERTA";
+
+        return "Valor: " + valor + " | " + "Data: " + data + mensagem;
+    }
 }
